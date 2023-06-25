ef7a718eeaadd815394be1b2e565dc0a08ae99b9
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-06-25 12:15:17 -0400

    created switch statement for shapes

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const { Triangle, Square, Circle } = require("./lib/shapes");
 
-const characterLimit = 3;
-
-const validateLength = (input) => {
-  if (input.length <= characterLimit) {
-    return true;
-  } else {
-    return `Please enter up to ${characterLimit} characters.`;
-  }
-};
-
-const colorText = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$|^[a-zA-Z]+$/;
-
-inquirer
-  .prompt([
-    {
-      name: "text",
-      type: "input",
-      message: "Enter text:",
-      validate: validateLength,
-    },
-    {
-      name: "color",
-      message: "Enter a color (Or a hexadecimal number):",
-      validate: function (input) {
-        if (colorText.test(input)) {
-          return true;
-        } else {
-          return "Please enter a color (Or a hexadecimal number):";
-        }
+function userInput() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter text for logo:",
+        name: "text",
+        validate: (input) =>
+          input.length <= 3 || "Enter no more than 3 characters.",
       },
-    },
-    {
-      name: "shape",
-      type: "list",
-      message: "Choose a shape:",
-      choices: ["circle", "triangle", "square"],
-    },
-    {
-      name: "shapeColor",
-      type: "input",
-      message: "Enter the shape color (keyword or hexadecimal):",
-    },
-  ])
-  .then((answers) => {
-    console.log("Text:", answers.text);
-    console.log("Color:", answers.color);
-    console.log("Shape:", answers.shape);
-    console.log("Shape Color:", answers.shapeColor);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+      {
+        type: "input",
+        message: "Choose the text color (Or a hexadecimal number)",
+        name: "textColor",
+      },
+      {
+        type: "list",
+        message: "Choose logo shape:",
+        choices: ["Triangle", "Square", "Circle"],
+        name: "shape",
+      },
+      {
+        type: "input",
+        message: "Choose the shape color (Or a hexadecimal number)",
+        name: "shapeColor",
+      },
+    ])
+    .then((answers) => {
+      let shape;
+      let svgText =
+        '<svg version="1.5.2" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+
+      switch (answers.shape) {
+        case "Triangle":
+          shape = new Triangle();
+          svgText += `<polygon points="150, 20 255, 185 60, 185" fill="${answers.shapeColor}"/>`;
+          break;
+        case "Square":
+          shape = new Square();
+          svgText += `<rect x="75" y="45" width="165" height="165" fill="${answers.shapeColor}"/>`;
+          break;
+        case "Circle":
+          shape = new Circle();
+          svgText += `<circle cx="155" cy="120" r="80" fill="${answers.shapeColor}"/>`;
+          break;
+      }
+    });
+}
+userInput();
