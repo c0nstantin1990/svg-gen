33ada10592bb040c879d68678fba7eb851eb6d2e
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-06-26 17:21:40 -0400

    error handling & made more readable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const { Triangle, Square, Circle } = require("./lib/shapes");
+
 // Function to validate hexadecimal number
 function validateColor(input) {
   const hexRegex = /^#([0-9A-Fa-f]{3}){1,2}$/;
@@ -11,16 +12,48 @@ function validateColor(input) {
     "Enter a valid color."
   );
 }
+
 // Function to write svg file
 function writeToFile(fileName, svgText) {
   fs.writeFile(fileName, svgText, { encoding: "utf8" }, (err) => {
     if (err) {
-      console.log(err);
+      console.log("Error writing file:", err);
     } else {
       console.log("Generated logo.svg");
     }
   });
 }
+
+// Generate SVG logo based on user input
+function generateLogo(text, textColor, shape, shapeColor) {
+  let shapeInstance;
+  let svgText =
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+
+  switch (shape) {
+    case "Triangle":
+      shapeInstance = new Triangle();
+      svgText += `<polygon points="150, 18 244, 182 56, 182" fill="${shapeColor}"/>`;
+      break;
+    case "Square":
+      shapeInstance = new Square();
+      svgText += `<rect x="73" y="40" width="160" height="160" fill="${shapeColor}"/>`;
+      break;
+    case "Circle":
+      shapeInstance = new Circle();
+      svgText += `<circle cx="150" cy="115" r="80" fill="${shapeColor}"/>`;
+      break;
+    default:
+      console.log("Invalid shape selected.");
+      return;
+  }
+
+  svgText += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${textColor}">${text}</text>`;
+  svgText += "</svg>";
+
+  writeToFile("logo.svg", svgText);
+}
+
 // User inputs for svg creation
 function userInput() {
   inquirer
@@ -52,31 +85,16 @@ function userInput() {
       },
     ])
     .then((answers) => {
-      let shape;
-      let svgText =
-        '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
-
-      switch (answers.shape) {
-        case "Triangle":
-          // Create a new instance of the Triangle shape
-          shape = new Triangle();
-          svgText += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}"/>`;
-          break;
-        case "Square":
-          // Create a new instance of the Square shape
-          shape = new Square();
-          svgText += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeColor}"/>`;
-          break;
-        case "Circle":
-          // Create a new instance of the Circle shape
-          shape = new Circle();
-          svgText += `<circle cx="150" cy="115" r="80" fill="${answers.shapeColor}"/>`;
-          break;
-      }
-      svgText += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
-      svgText += "</svg>";
-
-      writeToFile("logo.svg", svgText);
+      generateLogo(
+        answers.text,
+        answers.textColor,
+        answers.shape,
+        answers.shapeColor
+      );
+    })
+    .catch((error) => {
+      console.log("An error occurred:", error);
     });
 }
+
 userInput();
